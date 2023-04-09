b0fc202c41da7af617ad351786237ee99715d930
OGBASH03 <116046076+OGBASH03@users.noreply.github.com>
2023-04-09 12:05:51 -0400

    Engineer and Manager done

---
+++ tests/Engineer.test.js
@@ -0,0 +1,44 @@
+//DATA FROM ENGINNER JS FILE
+const Engineer = require("../lib/Engineer.js");
+// jest.mock("../lib/Engineer.js");
+
+test("creates an object for the engineer", () => {
+    const engineer = new Engineer("Bob", 10, "Bob@gmail.com", "Bobshack");
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+    expect(engineer.role).toEqual(expect.any(String));
+})
+
+test("retrieves the engineer name", () => {
+    const engineer = new Engineer("Bob", 10, "Bob@gmail.com", "Bobshack");
+
+    expect(engineer.retrieveName()).toBe("Bob");
+});
+
+test("retrieves the engineer id", () => {
+    const engineer = new Engineer("Bob", 10, "Bob@gmail.com", "Bobshack");
+
+    expect(engineer.retrieveId()).toBe(10);
+});
+
+test("retrieves the engineer email", () => {
+    const engineer = new Engineer("Bob", 10, "Bob@gmail.com", "Bobshack");
+
+    expect(engineer.retrieveEmail()).toBe("Bob@gmail.com");
+});
+
+test("retrieves the engineer role", () => {
+    const engineer = new Engineer("Bob", 10, "Bob@gmail.com", "Bobshack");
+
+    expect(engineer.retrieveRole()).toBe("Engineer");
+    console.log(engineer);
+});
+
+test("retrieves the engineer github", () => {
+    const engineer = new Engineer("Bob", 90, "Bob@gmail.com", "Bobshack");
+
+    expect(engineer.retrieveGithub()).toBe("Bobshack");
+})
---
+++ tests/Manager.test.js
@@ -0,0 +1,38 @@
+// DATA FROM MANAGER JS FILE
+const Manager = require("../lib/Manager.js");
+// jest.mock("../lib/Manager.js");
+
+test("creates an object for the manager", () => {
+    const manager = new Manager("Bob", 10, "Bob@gmail.com", 30);
+
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.role).toEqual(expect.any(String));
+})
+
+test("retrieves the manager name", () => {
+    const manager = new Manager("Bob", 10, "Bob@gmail.com", 30);
+
+    expect(manager.retrieveName()).toBe("Bob");
+});
+
+test("retrieves the manager id", () => {
+    const manager = new Manager("Bob", 10, "Bob@gmail.com", 30);
+
+    expect(manager.retrieveId()).toBe(10);
+});
+
+test("retrieves the manager email", () => {
+    const manager = new Manager("Bob", 10, "Bob@gmail.com", 30);
+
+    expect(manager.retrieveEmail()).toBe("Bob@gmail.com", 30);
+});
+
+test("retrieves the manager role", () => {
+    const manager = new Manager("Bob", 10, "Bob@gmail.com", 30);
+
+    expect(manager.retrieveRole()).toBe("Manager");
+    console.log(manager);
+});
